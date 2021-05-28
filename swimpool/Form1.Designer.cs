b011e224e90770cb6c8dc136b8ed2d68df927c27
Zhantore <49406760+zhanta@users.noreply.github.com>
2021-05-28 18:56:49 +0600

    Add files via upload

---
+++ swimpool/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Swimpool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Height";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Length";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(276, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(498, 276);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(119, 194);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 21);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Подогрев";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(119, 233);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 21);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Фонари";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Расчет расхода при стройке бассейна";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "м";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(226, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "м";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(225, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "м";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(119, 269);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(151, 21);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "Фильтр с насосом";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ swimpool/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swimpool
+{
+
+    public partial class Form1 : Form
+    {
+
+        int beton = 14000; // цена 14000 1куб
+        int zakazBetona = 30000; // цена заказа бетона 30 000 - 40 000
+        
+        double wPool; // ширина бассейна
+        double lPool; // длина бассейна
+        double hPool;
+        double cenaDoski = 150 * 1000; // кол Досок примерный +-
+        int sim = 2000*12; // сым акшасы
+
+        Boolean podogrevPool; // Будет еще насос, фонарики скиммер лестница или ступеньки.
+        Boolean fonari;
+        Boolean filter;
+        double ElectricProvoda = 15000; // цена для одной пачки(ну хз пока)
+        int workers = 8000000; // Цена для услуги
+
+
+        public static double numArmatura(double kg) // штук арматуры
+        {
+            return (81.6 * kg) / 1000; // 81,6шт - 1 тонна арматуры это 14мм 
+        }
+
+        public static double cenaArmatura(int kg)
+        {
+            return (237820*kg) / 1000; // 237820тг - 1т цена в интернете
+        }
+
+        public static double vBetonPool(double w, double l, double h)
+        { // объем бетона для бассейна
+            return (w * l * h) / 4;
+        }
+        public static double vPool(double w, double l, double h)
+        {
+            return w * l * h;
+        }
+        public static double sumKgArma(double v) // 15 для ленточного бетона
+        {
+            return 20 * v; // возвращает кг Арматур для бетона
+        }
+        public static double numOfPlitki(double w, double l, double h) // 16000 за кв.м
+        {
+            double Ss, Sp;
+            Ss = (2 * (w + l)) * h; // площадь весь бассейна
+            Sp = 0.3 * 0.2; // площадь 1й плитки
+            double numPlitki = Ss / Sp;
+            int zapasPlitki = (int)numPlitki / 10;
+            return numPlitki + zapasPlitki; // кол плиток с запасом
+        }
+
+        public static double cenaPlitki(double w, double l, double h)
+        {
+            double Ss = (2 * (w + l)) * h;
+            return 9000 * Ss;
+        }
+
+        public static double IzoMaterials(double Ss)
+        {
+            double S_izo_Mat = 1.2 * 1;
+            double Ss_total = Ss + (Ss / 4);
+            double numIzoMat = Ss_total / S_izo_Mat;
+            return numIzoMat;
+        }
+        public static double setkiMaterials(double Ss)
+        {
+            double S_setka_Mat = 0.7 * 0.8;
+            double Ss_total = Ss + (Ss / 4);
+            double numSetkiMat = Ss_total / S_setka_Mat;
+            return numSetkiMat;
+        }
+        public static double cenaIzoSetkiMaterials(double w, double l, double h) //2500тг 1кв м
+        {
+            double S1 = l*w;
+            double S2 = h * l;
+            double S3 = h * w;
+            return 2500 * (S1 + S2 + S3+(S1/10+S2/10+S3/10));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Расчитать
+        {
+            hPool = Convert.ToDouble(textBox1.Text);
+            wPool = Convert.ToDouble(textBox3.Text);
+            lPool = Convert.ToDouble(textBox2.Text);
+
+            if (checkBox1.Checked)
+                podogrevPool = true;
+            else
+                podogrevPool = false;
+            if (checkBox2.Checked)
+                fonari = true;
+            else
+                fonari = false;
+            if (checkBox3.Checked)
+                filter = true;
+            else
+                filter = false;
+
+            double obBetonPool = vBetonPool(wPool, lPool, hPool);
+            double obPool = vPool(wPool, lPool, hPool);
+            double cenaBeton = 14000 * obBetonPool; //Бетон цена
+            Int32 numArma = Convert.ToInt32(numArmatura(sumKgArma(obBetonPool)));
+            double cenaArma = cenaArmatura((int)sumKgArma(obBetonPool)); //Арматура цена
+            double kgArma = sumKgArma(obBetonPool);
+            Int32 numIzoMat = Convert.ToInt32(IzoMaterials(obPool));
+            Int32 numSetkiMat = Convert.ToInt32(setkiMaterials(obPool));
+            double cenaIzoSetki = cenaIzoSetkiMaterials(wPool, lPool, hPool);
+            double cenaFilter = 0;
+            double cenaFonari = 0;
+            double cenaPodogrevatel = 0;
+            double cenaPlitkov = cenaPlitki(wPool, lPool, hPool);
+            double cenaTruby = 2000 *(wPool + lPool + hPool); //Примерный затрат поскольку не знаешь сколько потребуется
+            listBox1.Items.Add("Бетон: " + obBetonPool +
+                "- объем, " + cenaBeton + "тг за бетон, за доставку " + zakazBetona + "тг");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Арматура: " + numArma + "шт, " + kgArma + "кг, " + cenaArma + "тг;");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Изо. Материалы: " + numIzoMat + "шт; Сетки: " + numSetkiMat + "шт; Общ цена: "
+                + cenaIzoSetki + "тг");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Трубы: " + cenaTruby + "тг;");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Плитки: " + numOfPlitki(wPool,lPool,hPool)+"шт; " + cenaPlitkov  +" тг");
+            listBox1.Items.Add("Пиломатериалы: "+ cenaDoski+"тг;");
+
+            if (lPool >= 8 && wPool >= 4)
+                workers = 8000000;
+            else if (lPool < 8 && lPool >= 6 && wPool < 4 && wPool >= 3)
+                workers = 6500000;
+            else
+                workers = 5000000;
+
+            if (filter)
+            {
+                if (lPool >= 8)
+                {
+                    cenaFilter = 120000;
+                    listBox1.Items.Add("Фильтр: " + cenaFilter + " тг;");
+                }
+                else
+                {
+                    cenaFilter = 65000;
+                    listBox1.Items.Add("Фильтр: " + cenaFilter + " тг;");
+                }
+            }
+            else
+                listBox1.Items.Add("Фильтр: нету");
+            if (fonari)
+            {
+                if (lPool >= 8)
+                {
+                    cenaFonari = 6 * 25000;
+                    listBox1.Items.Add("Фонари: 6шт, " + cenaFonari +"тг;");
+                }
+                else
+                {
+                    cenaFonari = 4 * 15000;
+                    listBox1.Items.Add("Фонари: 4шт, " + cenaFonari + "тг;");
+                }
+            }
+            else
+            {
+                listBox1.Items.Add("Фонари: нету");
+            }
+            if(podogrevPool)
+            {
+                cenaPodogrevatel = 95000;
+                listBox1.Items.Add("Подогреватель: " + cenaPodogrevatel + "тг;");
+            }
+            else
+                listBox1.Items.Add("Подогреватель: нету");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("Плата за услугу:" + workers + "тг");
+            listBox1.Items.Add("---------------------------------------------------------");
+            double obshCena = cenaBeton + zakazBetona + cenaArma + cenaIzoSetki + cenaTruby + cenaPlitkov + workers+sim + cenaFilter+cenaFonari+cenaPodogrevatel + cenaDoski;
+            listBox1.Items.Add("Общ. цена: " + obshCena +"тг.");
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)//Height
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)//Length
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)//Width
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)//Подогрев
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)// Фонари
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) //listBox
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
